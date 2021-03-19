0746bb96566cd9ea7aabd943948ca7060c6dea0d
Bharti0294 <bharti.nehul07@gmail.com>
2021-03-19 10:58:11 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import {InfoComponent} from './info/info.component';
+import {ContactusComponent} from './contactus/contactus.component';
+import {AboutComponent} from './about/about.component';
+import {ProfileComponent} from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"home",
+    component:HomeComponent
+  },
+
+  {
+    path:"info",
+    component:InfoComponent
+  },
+  
+  {
+    path:"contactus",
+    component:ContactusComponent
+  },
+  
+  {
+    path:"about",
+    component:AboutComponent
+  },
+  
+  {
+    path:"profile",
+    component:ProfileComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InfoComponent } from './info/info.component';
+
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    
+    ProfileComponent,
+    HomeComponent,
+    ContactusComponent,
+    
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
